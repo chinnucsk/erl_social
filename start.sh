bbf9cbe73d0865d2bc6b2f9b03e11f73aeae50cf
lucas <lucas@yun.io>
2013-08-16 11:28:43 +0800

    Add lib_dirs

--- start.sh
+++ start.sh
@@ -3,11 +3,13 @@
 CONFIG=priv/app.config
 NODE=erl_social@127.0.0.1
 COOKIE=erl_social
+LIB_DIRS="deps"
 
 exec erl -pa ebin \
 		-boot start_sasl \
 		-env ERL_LIBS "deps" \
 		-config $CONFIG\
+		-env ERL_LIBS $LIB_DIRS\
 		-name $NODE \
 		-s erl_social \
 		-s sync go \
